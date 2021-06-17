323eaa9339c0f591343cb3fa29699a7727b3f770
luandevpro <luandevpro@gmail.com>
2021-06-17 21:48:56 +0700

    v1.0.1

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("Hello world!");
+});
+
+app.listen(5000, () => {
+  console.log("run server success");
+});
